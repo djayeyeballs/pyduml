34cacf9b82e2e86966b51a9d23b5b7389e24aa64
MavProxyUser <kf@digitalmunition.com>
2017-07-13 04:26:35 -0400

    Add ARGV usage

--- DUMLHerring.rb
+++ DUMLHerring.rb
@@ -73,7 +73,7 @@ def crc16(buf,crc=0)
     return crc
 end
 
-crc16("Deeznuts")
+crc16("Deeznuts", "Deeznuts".length)
 
 #Product ID: 0x001f
 #  Vendor ID:    0x2ca3
@@ -115,11 +115,13 @@ sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 p1 = "\x55\x16\x04\xFC\x2A\x28\x65\x57\x40\x00\x07\x00\x00\x00\x00\x00\x00\x00\x00\x00\x27\xD3"
 sp.write p1
 #puts "0x7:received cmd to request enter upgrade mode, peer_id=0xa01, this_host=0x801"
+sleep(2)
 
 # Enable Reporting - report status:  0:0
 p2 = "\x55\x0E\x04\x66\x2A\x28\x68\x57\x40\x00\x0C\x00\x88\x20"
-#sp.write p2
+sp.write p2
 #puts "report status:  0:0"
+sleep(2)
 
 # Drop upgrade package. 
 require 'net/ftp'
@@ -128,9 +130,12 @@ ftp.passive = true
 ftp.login("RedHerring","IsDaRealest!" )
 puts "Logged into the FTPD"
 begin
-    firmware = File.new("fireworks.tar")
+#    firmware = File.new("fireworks.tar")
+    firmware = File.new(ARGV[0])
     puts "Dropping the hot sauce"
     ftp.putbinaryfile(firmware, "/upgrade/dji_system.bin")
+    puts ftp.ls("/upgrade/dji_system.bin")
+    puts ftp.ls("/upgrade/.bin/")
     puts "File upload is done"
 rescue Net::FTPPermError
     puts "failsauce"
@@ -151,7 +156,8 @@ ftp.close
 # The *known* size 111159808 aka "\x00\x2A\xA0\x06" goes with V01.03.0800_Mavic_dji_system.bin
 
 #size = File.size?("V01.03.0900_Mavic_dji_system.bin")
-size = File.size?("V01.03.0800_Mavic_dji_system.bin")
+#size = File.size?("V01.03.0800_Mavic_dji_system.bin")
+size = File.size?(ARGV[0])
 puts "File size is #{size}"
  
 size = Array(size).pack('V')
@@ -162,19 +168,20 @@ size = size.to_s.force_encoding('UTF-8')
 
 p3_pre =  "\x55\x1A\x04\xB1\x2A\x28\x6B\x57\x40\x00\x08\x00" + size                               + "\x00\x00\x00\x00\x00\x00\x02\x04" 
 
-crc = crc16(p3_pre)
-crc = Array(crc)
+#crc = crc16(p3_pre, p3_pre.length)
+#crc = Array(crc)
 #.pack('V')
-puts "Crc: #{crc}"
 
 crc = "\x72\x43"
 p3 = p3_pre + crc
+puts "Crc: #{crc}"
 sp.write p3
 #puts "f0x8:whole image size: 111159808, path = 2, type = 4"
+sleep(2)
 
 # File Verification and Start Upgrade - "0xa:Receive transfer complete message."
 p4 = "\x55\x1E\x04\x8A\x2A\x28\xF6\x57\x40\x00\x0A\x00\x66\x02\xC2\x6E\xD0\x72\x95\x81\x24\x68\x53\xD7\xC9\x88\xA4\xAE\x7A\xE4"
-#sp.write p4
+sp.write p4
 #puts "0xa:Receive transfer complete message."
 
 # To debug use: busybox tail -f /ftp/upgrade/dji/log/upgrade00.log 
